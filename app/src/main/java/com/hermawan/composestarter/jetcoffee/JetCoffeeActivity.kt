e287f34b14737308a3bbffcbf49153a3ee943ba5
Aldy Hermawan <aldyjonkunimen@gmail.com>
2022-11-02 17:03:48 +0800

    Add JetCoffe Category

--- app/src/main/java/com/hermawan/composestarter/jetcoffee/JetCoffeeActivity.kt
+++ app/src/main/java/com/hermawan/composestarter/jetcoffee/JetCoffeeActivity.kt
@@ -4,17 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hermawan.composestarter.R
+import com.hermawan.composestarter.jetcoffee.model.dummyCategory
+import com.hermawan.composestarter.jetcoffee.ui.components.CategoryItem
 import com.hermawan.composestarter.jetcoffee.ui.components.SearchBar
+import com.hermawan.composestarter.jetcoffee.ui.components.SectionText
 import com.hermawan.composestarter.jetcoffee.ui.theme.ComposeStarterTheme
 
 class JetCoffeeActivity : ComponentActivity() {
@@ -33,6 +42,8 @@ class JetCoffeeActivity : ComponentActivity() {
 fun JetCoffeeApp() {
     Column {
         Banner()
+        SectionText(title = stringResource(id = R.string.section_category))
+        CategoryRow()
     }
 }
 
@@ -48,6 +59,21 @@ fun Banner(
             modifier = Modifier.height(160.dp)
         )
         SearchBar()
+    }
+}
+
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(dummyCategory, key = { it.textCategory }) {
+            CategoryItem(category = it)
+        }
     }
 }
 
