7b614650321b3f779798c68019f306ec8141a122
Aldy Hermawan <aldyjonkunimen@gmail.com>
2022-11-02 20:07:59 +0800

    Implement BottomNav

--- app/src/main/java/com/hermawan/composestarter/jetcoffee/JetCoffeeActivity.kt
+++ app/src/main/java/com/hermawan/composestarter/jetcoffee/JetCoffeeActivity.kt
@@ -9,10 +9,21 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -21,6 +32,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hermawan.composestarter.R
+import com.hermawan.composestarter.jetcoffee.model.BottomBarItem
 import com.hermawan.composestarter.jetcoffee.model.Menu
 import com.hermawan.composestarter.jetcoffee.model.dummyBestSellerMenu
 import com.hermawan.composestarter.jetcoffee.model.dummyCategory
@@ -30,6 +42,7 @@ import com.hermawan.composestarter.jetcoffee.ui.components.HomeSection
 import com.hermawan.composestarter.jetcoffee.ui.components.MenuItem
 import com.hermawan.composestarter.jetcoffee.ui.components.SearchBar
 import com.hermawan.composestarter.jetcoffee.ui.theme.ComposeStarterTheme
+import com.hermawan.composestarter.jetcoffee.ui.theme.LightGray
 
 class JetCoffeeActivity : ComponentActivity() {
 
@@ -44,18 +57,26 @@ class JetCoffeeActivity : ComponentActivity() {
 }
 
 @Composable
-fun JetCoffeeApp() {
-    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-        Banner()
-        HomeSection(
-            title = stringResource(id = R.string.section_category),
-            content = { CategoryRow() }
-        )
-        HomeSection(title = stringResource(id = R.string.section_favorite_menu), Modifier, {
-            MenuRow(listMenu = dummyMenu)
-        })
-        HomeSection(title = stringResource(id = R.string.section_best_seller_menu)) {
-            MenuRow(listMenu = dummyBestSellerMenu)
+fun JetCoffeeApp(modifier: Modifier = Modifier) {
+    Scaffold(
+        bottomBar = { BottomBar() }
+    ) {
+        Column(
+            modifier = modifier
+                .verticalScroll(rememberScrollState())
+                .padding(it)
+        ) {
+            Banner()
+            HomeSection(
+                title = stringResource(id = R.string.section_category),
+                content = { CategoryRow() }
+            )
+            HomeSection(title = stringResource(id = R.string.section_favorite_menu), Modifier, {
+                MenuRow(listMenu = dummyMenu)
+            })
+            HomeSection(title = stringResource(id = R.string.section_best_seller_menu)) {
+                MenuRow(listMenu = dummyBestSellerMenu)
+            }
         }
     }
 }
@@ -102,6 +123,45 @@ fun MenuRow(
     ) {
         items(listMenu, key = { it.title }) {
             MenuItem(menu = it)
+        }
+    }
+}
+
+@Composable
+fun BottomBar(
+    modifier: Modifier = Modifier
+) {
+    BottomNavigation(
+        backgroundColor = MaterialTheme.colors.background,
+        contentColor = MaterialTheme.colors.primary,
+        modifier = modifier
+    ) {
+        val navigationItems = listOf(
+            BottomBarItem(
+                title = stringResource(id = R.string.menu_home),
+                icon = Icons.Default.Home
+            ),
+            BottomBarItem(
+                title = stringResource(R.string.menu_favorite),
+                icon = Icons.Default.Favorite
+            ),
+            BottomBarItem(
+                title = stringResource(R.string.menu_profile),
+                icon = Icons.Default.AccountCircle
+            ),
+        )
+        navigationItems.map {
+            BottomNavigationItem(
+                icon = {
+                    Icon(imageVector = it.icon, contentDescription = it.title)
+                },
+                label = {
+                    Text(text = it.title)
+                },
+                selected = it.title == navigationItems[0].title,
+                unselectedContentColor = LightGray,
+                onClick = { /*TODO*/ }
+            )
         }
     }
 }
---
+++ app/src/main/java/com/hermawan/composestarter/jetcoffee/model/BottomBarItem.kt
@@ -0,0 +1,8 @@
+package com.hermawan.composestarter.jetcoffee.model
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomBarItem(
+    val title: String,
+    val icon: ImageVector,
+)
