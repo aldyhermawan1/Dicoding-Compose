70860bf86c1aac7ea3bc21841152d1fc1449537b
Aldy Hermawan <aldyjonkunimen@gmail.com>
2022-11-09 19:09:38 +0800

    Add Nav Drawer

---
+++ app/src/main/java/com/hermawan/composestarter/navdrawer/NavDrawerActivity.kt
@@ -0,0 +1,221 @@
+package com.hermawan.composestarter.navdrawer
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.SnackbarResult.ActionPerformed
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hermawan.composestarter.R
+import com.hermawan.composestarter.navdrawer.ui.theme.ComposeStarterTheme
+import kotlinx.coroutines.launch
+
+class NavDrawerActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeStarterTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    MyNavDrawerApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyNavDrawerApp() {
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            MyTopBar {
+                scope.launch {
+                    scaffoldState.drawerState.open()
+                }
+            }
+        },
+        drawerContent = {
+            MyDrawerContent(
+                onItemSelected = {
+                    scope.launch {
+                        scaffoldState.drawerState.close()
+                        val snackbarResult = scaffoldState.snackbarHostState.showSnackbar(
+                            message = context.resources.getString(R.string.coming_soon, it),
+                            actionLabel = context.resources.getString(R.string.subscribe_question)
+                        )
+                        if (snackbarResult == ActionPerformed) {
+                            Toast.makeText(
+                                context,
+                                context.resources.getString(R.string.subscribed_info),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                },
+                onBackPressed = {
+                    if (scaffoldState.drawerState.isOpen) {
+                        scope.launch {
+                            scaffoldState.drawerState.close()
+                        }
+                    }
+                }
+            )
+        },
+        drawerGesturesEnabled = scaffoldState.drawerState.isOpen
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = stringResource(id = R.string.hello_world))
+        }
+    }
+}
+
+@Composable
+fun MyTopBar(onMenuClick: () -> Unit) {
+    TopAppBar(
+        navigationIcon = {
+            IconButton(onClick = { onMenuClick() }) {
+                Icon(imageVector = Icons.Default.Menu, contentDescription = stringResource(id = R.string.menu))
+            }
+        },
+        title = {
+            Text(text = stringResource(id = R.string.app_name_navdrawer))
+        }
+    )
+}
+
+@Composable
+fun MyDrawerContent(
+    modifier: Modifier = Modifier,
+    onItemSelected: (title: String) -> Unit,
+    onBackPressed: () -> Unit
+) {
+    val items = listOf(
+        MenuItem(
+            title = stringResource(id = R.string.home),
+            icon = Icons.Default.Home
+        ),
+        MenuItem(
+            title = stringResource(R.string.favourite),
+            icon = Icons.Default.Favorite
+        ),
+        MenuItem(
+            title = stringResource(R.string.profile),
+            icon = Icons.Default.AccountCircle
+        ),
+    )
+
+    Column(modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .height(190.dp)
+                .fillMaxWidth()
+                .background(MaterialTheme.colors.primary)
+        )
+        for (item in items) {
+            Row(
+                modifier = Modifier
+                    .clickable { onItemSelected(item.title) }
+                    .padding(vertical = 12.dp, horizontal = 16.dp)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = item.icon, contentDescription = item.title, tint = Color.DarkGray)
+                Spacer(modifier = Modifier.width(32.dp))
+                Text(text = item.title, style = MaterialTheme.typography.subtitle2)
+            }
+            Divider()
+        }
+    }
+    BackPressHandler {
+        onBackPressed()
+    }
+}
+
+@Composable
+fun BackPressHandler(enabled: Boolean = true, onBackPressed: () -> Unit) {
+    val currentOnBackPressed by rememberUpdatedState(newValue = onBackPressed)
+    val backCallback = remember {
+        object : OnBackPressedCallback(enabled) {
+            override fun handleOnBackPressed() {
+                currentOnBackPressed()
+            }
+        }
+    }
+
+    SideEffect {
+        backCallback.isEnabled = enabled
+    }
+
+    val backDispatcher = checkNotNull(LocalOnBackPressedDispatcherOwner.current) {
+        "No OnBackPressedDispatcherOwner was provided via LocalOnBackPressedDispatcherOwner"
+    }.onBackPressedDispatcher
+
+    val lifecycleOwner = LocalLifecycleOwner.current
+    DisposableEffect(lifecycleOwner, backDispatcher) {
+        backDispatcher.addCallback(lifecycleOwner, backCallback)
+        onDispose { backCallback.remove() }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NavDrawerPreview() {
+    ComposeStarterTheme {
+        MyNavDrawerApp()
+    }
+}
+
+data class MenuItem(val title: String, val icon: ImageVector)
