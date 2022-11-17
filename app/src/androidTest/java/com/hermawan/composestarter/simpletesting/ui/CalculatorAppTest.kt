debb000febecaf263c3bf341b4f24e7526ac295d
Aldy Hermawan <aldyjonkunimen@gmail.com>
2022-11-17 10:06:36 +0800

    Refactor to Implement Get String Resources

--- app/src/androidTest/java/com/hermawan/composestarter/simpletesting/ui/CalculatorAppTest.kt
+++ app/src/androidTest/java/com/hermawan/composestarter/simpletesting/ui/CalculatorAppTest.kt
@@ -1,15 +1,18 @@
 package com.hermawan.composestarter.simpletesting.ui
 
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
+import com.hermawan.composestarter.R
 import com.hermawan.composestarter.temperature.ui.theme.ComposeStarterTheme
 import org.junit.*
 
 class CalculatorAppTest {
+
     @get:Rule
-    val composeTestRule = createComposeRule()
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
     @Before
     fun setUp() {
@@ -22,9 +25,21 @@ class CalculatorAppTest {
 
     @Test
     fun calculate_area_of_rectangle_correct() {
-        composeTestRule.onNodeWithText("Masukkan panjang").performTextInput("3")
-        composeTestRule.onNodeWithText("Masukkan lebar").performTextInput("4")
-        composeTestRule.onNodeWithText("Hitung!").performClick()
-        composeTestRule.onNodeWithText("Hasil: 12.0").assertExists()
+        with(composeTestRule) {
+            onNodeWithText(activity.getString(R.string.enter_length)).performTextInput("3")
+            onNodeWithText(activity.getString(R.string.enter_width)).performTextInput("4")
+            onNodeWithText(activity.getString(R.string.count)).performClick()
+            onNodeWithText(activity.getString(R.string.result, "12.0")).assertExists()
+        }
+    }
+
+    @Test
+    fun wrong_input_not_calculated() {
+        with(composeTestRule) {
+            onNodeWithText(activity.getString(R.string.enter_length)).performTextInput("..3")
+            onNodeWithText(activity.getString(R.string.enter_width)).performTextInput("4")
+            onNodeWithText(activity.getString(R.string.count)).performClick()
+            onNodeWithText(activity.getString(R.string.result, "0.0")).assertExists()
+        }
     }
 }
